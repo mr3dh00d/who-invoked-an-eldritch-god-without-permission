cb57caad2053946f2f4598d07696fd356f98f668
Dereck Pavez <d.pavezarias@uandresbello.edu>
2023-06-06 03:22:59 -0400

    Mercader funcionando con dialogos

---
+++ Assets/Scripts/NPC's/Trader/Mercader.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class Mercader : MonoBehaviour
+{
+    [SerializeField] private GameObject actionMark;
+    [SerializeField] private Sprite playerSprite;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private Image dialogueImage;
+    [SerializeField, TextArea(4,6)] private string[] dialogueLines;
+    private float typingSpeed = 0.05f;
+    private bool isPlayerInRange;
+    private bool isDialogueActive;
+    private bool didDialogueStarted;
+    private int lineIndex;
+    private Animator MercaderAnimation;
+    private Sprite mercaderSprite;
+    // Update is called once per frame
+
+    void Start()
+    {
+        MercaderAnimation = GetComponent<Animator>();
+        mercaderSprite = GetComponent<SpriteRenderer>().sprite;
+    }
+
+    void Update()
+    {
+        if(isPlayerInRange)
+        {
+            if(Input.GetKey("z")){
+                if(isDialogueActive)
+                {
+                    if(!didDialogueStarted)
+                    {
+                        StartDialogue();
+                    }
+                    if(didDialogueStarted)
+                    {
+                        if (dialogueText.text == dialogueLines[lineIndex])
+                        {
+                            NextLine();
+                        }
+                        // else if (dialogueText.text != dialogueLines[lineIndex])
+                        // {
+                        //     StopAllCoroutines();
+                        //     dialogueText.text = dialogueLines[lineIndex];
+                        // }
+                    }
+
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isDialogueActive = true;
+            isPlayerInRange = true;
+            actionMark.SetActive(true);
+            MercaderAnimation.SetBool("Active", true);
+            Debug.Log("si se pudo burro");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            actionMark.SetActive(false);
+            MercaderAnimation.SetBool("Active", false);
+            Debug.Log("te chingaste perro");
+        }
+    }
+    private void StartDialogue()
+    {
+        didDialogueStarted = true;
+        dialogueImage.sprite = mercaderSprite;
+        dialoguePanel.SetActive(true);
+        actionMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine());
+        // dialogueText.text = dialogueLines[0];
+    }
+    private void NextLine()
+    {
+        if(lineIndex < dialogueLines.Length - 1)
+        {
+            lineIndex++;
+            dialogueText.text = string.Empty;
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogueStarted = false;
+            isDialogueActive = false;
+            dialoguePanel.SetActive(false);
+            didDialogueStarted = false;
+            Time.timeScale = 1f;
+        }
+    }
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+
+        if(!(lineIndex%2==0))
+        {
+            dialogueImage.sprite = playerSprite;
+        }
+        else
+        {
+            dialogueImage.sprite = mercaderSprite;
+        }
+
+        foreach(char letter in dialogueLines[lineIndex].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSecondsRealtime(typingSpeed);
+        }
+    }
+    
+}

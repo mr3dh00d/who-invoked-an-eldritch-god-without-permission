9003248f6f77d10e17e5e6b58e71a608179769d4
Josefa Toro <54225936+Kopfsalaat@users.noreply.github.com>
2023-06-05 21:42:06 -0400

    Animaciones del Jugador

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -7,17 +7,22 @@ public class Movement : MonoBehaviour
     [SerializeField] public float speed = 5f;
     [SerializeField] private Vector2 direction;
     private Rigidbody2D rb;
+    private Animator playerAnimation;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Movement script is working");
         rb = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        playerAnimation.SetFloat("Horizontal", direction.x);
+        playerAnimation.SetFloat("Vertical", direction.y);
+        playerAnimation.SetFloat("Speed", direction.sqrMagnitude);
         // transform.position += move * speed * Time.deltaTime;
     }
 
